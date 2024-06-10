e23fd0eeb0748f8a3501b9fe55d822f0f34206de
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-06-10 09:32:53 +0700

    Golang Distributed

---
+++ p2p/transport.go
@@ -0,0 +1,12 @@
+package p2p
+
+// Peer is an interface that represents the remote node in the network.
+type Peer interface {
+}
+
+// Transport is anything that handles the communication.
+// between the node in the network. This can be of the 
+// form (TCP, UDP, Websocket, etc.)
+type Transport interface {
+
+}
